4bd98dea83a92857e2ceb4c0fa7f5b2fff568266
Ashok Amsamani <58943248+ashok-amsamani@users.noreply.github.com>
2020-10-18 16:26:33 +0530

    Create Hadoop.sh
    Run this script in linux terminal

---
+++ Hadoop.sh
@@ -0,0 +1,8 @@
+hadoop fs -du -h /user/hive/warehouse/usecase.db/staging_txn
+#total space occupied: 8.1 MB
+
+hadoop fs -du -h /user/hive/warehouse/usecase.db/txn_parquet
+#total space occupied: 1.3 MB
+
+hadoop fs -du -h /user/hive/warehouse/usecase.db/txn_orc
+#total space occupied: 943.3 K
